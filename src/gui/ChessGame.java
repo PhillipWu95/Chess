e32f74e439d326f528458c7a9fd6d1f9012b553e
Phillip <phillipwu95@gmail.com>
2019-10-02 11:12:00 +1000

    added resource images

--- src/gui/ChessGame.java
+++ src/gui/ChessGame.java
@@ -66,5 +66,71 @@ public class ChessGame {
 		background.add(boardView);
 		boardView.setLayout(new GridLayout(9, 9, 0, 0));
 		
+		JLabel lbH = new JLabel("h");
+		GridBagConstraints cH = new GridBagConstraints();
+		cH.fill = GridBagConstraints.BOTH;
+		cH.gridx = 0;
+		cH.gridy = 0;
+		boardView.add(lbH, cH);
+		
+		JLabel lbG = new JLabel("g");
+		GridBagConstraints cG = new GridBagConstraints();
+		cG.fill = GridBagConstraints.BOTH;
+		cG.gridx = 0;
+		cG.gridy = 1;
+		boardView.add(lbG, cG);
+		
+		JLabel lbF = new JLabel("f");
+		GridBagConstraints cF = new GridBagConstraints();
+		cF.fill = GridBagConstraints.BOTH;
+		cF.gridx = 0;
+		cF.gridy = 2;
+		boardView.add(lbF, cF);
+		
+		JLabel lbE = new JLabel("e");
+		GridBagConstraints cE = new GridBagConstraints();
+		cE.fill = GridBagConstraints.BOTH;
+		cE.gridx = 0;
+		cE.gridy = 3;
+		boardView.add(lbE, cE);
+		
+		JLabel lbD = new JLabel("d");
+		GridBagConstraints cD = new GridBagConstraints();
+		cD.fill = GridBagConstraints.BOTH;
+		cD.gridx = 0;
+		cD.gridy = 4;
+		boardView.add(lbD, cD);
+		
+		JLabel lbC = new JLabel("c");
+		GridBagConstraints cC = new GridBagConstraints();
+		cC.fill = GridBagConstraints.BOTH;
+		cC.gridx = 0;
+		cC.gridy = 5;
+		boardView.add(lbC, cC);
+		
+		JLabel lbB = new JLabel("b");
+		GridBagConstraints cB = new GridBagConstraints();
+		cB.fill = GridBagConstraints.BOTH;
+		cB.gridx = 0;
+		cB.gridy = 6;
+		boardView.add(lbB, cB);
+		
+		JLabel lbA = new JLabel("a");
+		GridBagConstraints cA = new GridBagConstraints();
+		cA.fill = GridBagConstraints.BOTH;
+		cA.gridx = 0;
+		cA.gridy = 7;
+		boardView.add(lbA, cA);
+		
+		JLabel lbEmpty = new JLabel("/");
+		GridBagConstraints cEmpty = new GridBagConstraints();
+		cEmpty.fill = GridBagConstraints.BOTH;
+		cEmpty.gridx = 0;
+		cEmpty.gridy = 1;
+		boardView.add(lbEmpty, cEmpty);
+		
+		
+		
+		
 	}
 }
